5e76f5d9ff34edc26af9330d615ee9f3efd4de1e
k a <kaywoz@outlook.com>
2022-11-21 12:33:56 +0100

    ?

---
+++ add-ons.ps1
@@ -0,0 +1,14 @@
+## basic stuff to add on or incorporate from various sources.
+
+<#Press Windows key + R
+Type regedit <enter> to open the registry
+Navigate to HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Service
+Change the following folders the key Start to 4
+Sense
+WdBoot
+WdFilter
+WdNisDrv
+WdNisSvc
+WinDefend
+
+Once done restart back to normal boot #>
